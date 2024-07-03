f6602c3f4091ac7e608f8860bb9bb2478c04e630
Erick <ericksalazar_g@hotmail.com>
2024-07-03 14:53:44 -0600

    adding Delete and Update Item funtions to my API

--- Repositories/IInMemItemsRepository.cs
+++ Repositories/IInMemItemsRepository.cs
@@ -24,5 +24,22 @@ namespace Catalog.Repositories
         public Item GetItem(Guid id){
             return items.Where(item => item.Id == id).FirstOrDefault();
         }
+
+        public void CreateItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == item.Id);
+            items[index] = item;
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items.RemoveAt(index);
+        }
     }
 }
